37087ffd20adbef4e7ad4e50aff2a7d39a436561
Riissss <121730144+Riissss@users.noreply.github.com>
2023-08-17 05:24:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.MONGO_DB_URI = "mongodb+srv://ohmyriiss:d1G1OlTFs3bJ0EuH@cluster0.0fqcgh8.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnagp+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "arispurwanto2822@gmail.com" //email
+global.email_password = "vtayhauepqbqvfhh" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
